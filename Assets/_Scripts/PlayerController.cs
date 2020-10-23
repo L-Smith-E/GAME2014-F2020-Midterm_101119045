098a90913ec6c2322748289d51d5486cb422bf70
Laurence <smith.e.laurence@gmail.com>
2020-10-22 22:52:36 -0400

    2nd Commit
    Background Scrolls
    Player on Left side
    Enemies move up and down on right side
    Bullets do not travel properly
    Player Movement is broken

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -11,10 +11,16 @@ public class PlayerController : MonoBehaviour
     [Header("Boundary Check")]
     public float horizontalBoundary;
 
+    [Header("Boundary Check")]
+    public float verticalBoundary;
+
     [Header("Player Speed")]
     public float horizontalSpeed;
     public float maxSpeed;
     public float horizontalTValue;
+    public float verticalSpeed;
+    public float verticalTValue;
+
 
     [Header("Bullet Firing")]
     public float fireDelay;
@@ -56,42 +62,64 @@ public class PlayerController : MonoBehaviour
         {
             var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
 
-            if (worldTouch.x > transform.position.x)
+            //if (worldTouch.x > transform.position.x)
+            //{
+            //    // direction is positive
+            //    direction = 1.0f;
+            //}
+
+            //if (worldTouch.x < transform.position.x)
+            //{
+            //    // direction is negative
+            //    direction = -1.0f;
+            //}
+            
+            if (worldTouch.y > transform.position.y)
             {
-                // direction is positive
                 direction = 1.0f;
             }
-
-            if (worldTouch.x < transform.position.x)
+            
+            if (worldTouch.y < transform.position.y)
             {
-                // direction is negative
                 direction = -1.0f;
             }
+
+            
 
             m_touchesEnded = worldTouch;
 
         }
 
         // keyboard support
-        if (Input.GetAxis("Horizontal") >= 0.1f) 
+        //if (Input.GetAxis("Horizontal") >= 0.1f) 
+        //{
+        //    // direction is positive
+        //    direction = 1.0f;
+        //}
+
+        //if (Input.GetAxis("Horizontal") <= -0.1f)
+        //{
+        //    // direction is negative
+        //    direction = -1.0f;
+        //}
+
+        if (Input.GetAxis("Vertical") >= 0.1f)
         {
-            // direction is positive
             direction = 1.0f;
         }
 
-        if (Input.GetAxis("Horizontal") <= -0.1f)
+        if (Input.GetAxis("Vertical") <= -0.1f)
         {
-            // direction is negative
             direction = -1.0f;
         }
 
         if (m_touchesEnded.x != 0.0f)
         {
-           transform.position = new Vector2(Mathf.Lerp(transform.position.x, m_touchesEnded.x, horizontalTValue), transform.position.y);
+           transform.position = new Vector2(Mathf.Lerp(transform.position.y, m_touchesEnded.y, verticalTValue), transform.position.y);
         }
         else
         {
-            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * verticalSpeed, 0.0f);
             m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
             m_rigidBody.velocity *= 0.99f;
         }
@@ -100,15 +128,15 @@ public class PlayerController : MonoBehaviour
     private void _CheckBounds()
     {
         // check right bounds
-        if (transform.position.x >= horizontalBoundary)
+        if (transform.position.y >= verticalBoundary)
         {
-            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+            transform.position = new Vector3(verticalBoundary, transform.position.y, 0.0f);
         }
 
         // check left bounds
-        if (transform.position.x <= -horizontalBoundary)
+        if (transform.position.y <= -verticalBoundary)
         {
-            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
+            transform.position = new Vector3(-verticalBoundary, transform.position.y, 0.0f);
         }
 
     }
